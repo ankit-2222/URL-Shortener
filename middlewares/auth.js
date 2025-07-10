7726f9e48f6980a042d4fdc88b642f945b359f61
ankit-2222 <ankitmahapatra8018@gmail.com>
2025-07-11 00:46:08 +0530

    final push

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,27 @@
 const { getUser } = require('../service/auth');
 
+function checkForAuthentication(req, res, next) {
+    const authorizationHeaderValue = req.headers["authorization"];
+    req.user = null;
+
+    if(!authorizationHeaderValue || !authorizationHeaderValue.startsWith("Bearer"))
+        return next();
+
+    const token = authorizationHeaderValue.split("Bearer ")[1];
+    const user = getUser(token);
+
+    req.user = user;
+    return next();
+}
+
+function restrictTo(roles = []) {
+    return function (req, res, next) {
+        if (!req.user) return res.redirect("/login");
+
+        if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+    }
+}
+
 function restrictToUserLoggedinOnly(req, res, next) {
     const userUid = req.cookies?.uid;
 
